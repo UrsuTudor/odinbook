116c8da6a87e49affb6e2f6046346b79eda49626
UrsuTudor <tudor_ursu90@yahoo.com>
2024-11-14 19:11:03 +0200

    Add User, Post models and controllers + root

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
-describe UsersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should return posts#index when user is signed_in" do
+    sign_in User.first
+    get root_path
+    assert_response :success
+  end
+
+  test "should redirect to sign_in when user is not logged in" do
+    get root_path
+    assert_redirected_to user_session_path
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,4 @@
 require "test_helper"
 
-describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+class UserTest < ActiveSupport::TestCase
 end
