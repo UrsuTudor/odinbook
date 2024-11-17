a88ce979ff7b9d9193c05bdca7f85a8f7d4a933f
Ursu Tudor <tudor_ursu90@yahoo.com>
2024-11-17 22:30:51 +0200

    Add tests for user#follow and user#unfollow

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ require "test_helper"
 class UsersControllerTest < ActionController::TestCase
   include Devise::Test::ControllerHelpers
 
+
   def setup
     @user = users(:one)
     @user2 = users(:two)
@@ -12,27 +13,25 @@ class UsersControllerTest < ActionController::TestCase
     @controller.params = { id: @user2.id }
   end
 
+
   test "user#follow and user#unfollow should add and remove the current user as a follower to another user" do
     assert_not @user2.followers.first
 
     @controller.follow
-
     assert_equal @user2.followers.first, @user
 
     @controller.unfollow
-
     assert_not @user2.followers.first
   end
+
 
   test "user#follow and user#unfollow should add and remove the other user as a followee to the current user" do
     assert_not @user.followees.first
 
     @controller.follow
-
     assert_equal @user.followees.first, @user2
 
     @controller.unfollow
-
     assert_not @user.followees.first
   end
 end
