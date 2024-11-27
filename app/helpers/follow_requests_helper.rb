20c01def5ed0d5979b6b19fb7f3e17dfd11cb70b
UrsuTudor <tudor_ursu90@yahoo.com>
2024-11-27 16:22:47 +0200

    Move the follow request form and following button into helper
    They have been moved into follow_requests_helper under the same
    consideration as the form that allowed for the acceptance or deletion
    of follow requests.

--- app/helpers/follow_requests_helper.rb
+++ app/helpers/follow_requests_helper.rb
@@ -6,4 +6,18 @@ module FollowRequestsHelper
       concat f.submit(button_text, data: { controller: "follow-buttons", action: stimulus_action, "follow-buttons-target": "button" })
     end
   end
+
+  def follow_request_form(user, sent_follow_requests)
+    form_with(url: follow_requests_path, method: :post) do |f|
+      concat user.name
+
+      if current_user.followees.include?(user)
+        concat f.submit "Following", disabled: true
+      elsif !sent_follow_requests.include?(user.id)
+        concat f.hidden_field :recipient_id, value: user.id
+        sent_follow_requests.include?(user.id)
+        concat f.submit "Send Follow Request", data: { controller: "follow-buttons", action: "click->follow-buttons#change_request_button", "follow-buttons-target": "button" }
+      end
+    end
+  end
 end
