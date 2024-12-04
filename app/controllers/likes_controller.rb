2cfa1741b15401c68cb780488f80bf9dbaa4cd1f
Ursu Tudor <tudor_ursu90@yahoo.com>
2024-12-04 22:22:39 +0200

    Fix a bug that would not update likes/buttons for posts
    Because I didn't give each post/like counter a unique id, only the first
    post's likes/buttons would get updated by turbo_stream, creating behavior
    that looked much scarier than it actually was.
    
    I fixed this by giving each post's divs their unique ids with ruby or erb.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -10,8 +10,8 @@ class LikesController < ApplicationController
       if @like.save
         format.turbo_stream do
           render turbo_stream: [
-            turbo_stream.replace("like_button", partial: "likes/unlike_form", locals: { post: @post, like: @like }),
-            turbo_stream.update("likes", @post.likes.count) ]
+            turbo_stream.replace("like_button_#{@post.id}", partial: "likes/unlike_form", locals: { post: @post, like: @like }),
+            turbo_stream.update("likes_#{@post.id}", @post.likes.count) ]
         end
       else
         format.html { redirect_to root_path, status: :unprocessable_entity }
@@ -26,8 +26,8 @@ class LikesController < ApplicationController
       if @like.destroy
         format.turbo_stream do
           render turbo_stream: [
-            turbo_stream.replace("like_button", partial: "likes/form", locals: { post: @post }),
-            turbo_stream.update("likes", @post.likes.count) ]
+            turbo_stream.replace("like_button_#{@post.id}", partial: "likes/form", locals: { post: @post }),
+            turbo_stream.update("likes_#{@post.id}", @post.likes.count) ]
         end
       else
         format.html { redirect_to root_path, status: :unprocessable_entity }
