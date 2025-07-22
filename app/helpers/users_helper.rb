77b6ef124e2d3b46d5fe1a80f50312cbd4eedbac
Ursu Tudor <tudor_ursu90@yahoo.com>
2025-07-22 21:05:28 +0300

    Add profile styling and reorganize main layout
    The main layout was reorganized to use flex instead of grid, as that
    makes positioning easier to handle

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,10 @@
 module UsersHelper
   def profile_picture_form
     if @user == current_user
-      form_with(model: @user, html: { multipart: true }, method: :patch) do |form|
-        concat form.file_field :profile_picture
-        concat form.submit "Upload profile picture"
+      form_with(model: @user, html: { multipart: true }, method: :patch, class: "profileForm") do |form|
+        concat form.label(:profile_picture, "Choose Profile Picture", for: "profile_picture_upload", class: "submitBtn")
+        concat form.file_field(:profile_picture, id: "profile_picture_upload", class: "hidden")
+        concat form.submit("Upload profile picture", class: "submitBtn")
       end
     end
   end
